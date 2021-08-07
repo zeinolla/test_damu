109b5858cfcec03e257e5a6fba8099965e73bf56
ddd <48688417+zeinolla@users.noreply.github.com>
2021-08-07 16:47:05 +0600

    dd/

---
+++ animations.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+       margin:10,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:1
+        }
+    }
+})});
